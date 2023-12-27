9a866820e663a864561a0e79d1664a70a42e21f7
Mike <mike@sinisterwaltz.com>
2023-12-27 11:01:48 +0000

    setup msw node

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,38 +1,43 @@
-import { PassThrough } from 'node:stream'
-import type { EntryContext } from '@remix-run/node'
-import { Response } from '@remix-run/node'
-import { RemixServer } from '@remix-run/react'
-import isbot from 'isbot'
-import { renderToPipeableStream } from 'react-dom/server'
+import { PassThrough } from 'node:stream';
+import type { EntryContext } from '@remix-run/node';
+import { Response } from '@remix-run/node';
+import { RemixServer } from '@remix-run/react';
+import isbot from 'isbot';
+import { renderToPipeableStream } from 'react-dom/server';
+import { server } from './mocks/node';
 
-const ABORT_DELAY = 5_000
+const ABORT_DELAY = 5_000;
+
+if (process.env.NODE_ENV === 'development') {
+  server.listen();
+}
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
+  remixContext: EntryContext
 ) {
   return isbot(request.headers.get('user-agent'))
     ? handleBotRequest(
         request,
         responseStatusCode,
         responseHeaders,
-        remixContext,
+        remixContext
       )
     : handleBrowserRequest(
         request,
         responseStatusCode,
         responseHeaders,
-        remixContext,
-      )
+        remixContext
+      );
 }
 
 function handleBotRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
+  remixContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
     const { pipe, abort } = renderToPipeableStream(
@@ -43,38 +48,38 @@ function handleBotRequest(
       />,
       {
         onAllReady() {
-          const body = new PassThrough()
+          const body = new PassThrough();
 
-          responseHeaders.set('Content-Type', 'text/html')
+          responseHeaders.set('Content-Type', 'text/html');
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
               status: responseStatusCode,
-            }),
-          )
+            })
+          );
 
-          pipe(body)
+          pipe(body);
         },
         onShellError(error: unknown) {
-          reject(error)
+          reject(error);
         },
         onError(error: unknown) {
-          responseStatusCode = 500
-          console.error(error)
+          responseStatusCode = 500;
+          console.error(error);
         },
-      },
-    )
+      }
+    );
 
-    setTimeout(abort, ABORT_DELAY)
-  })
+    setTimeout(abort, ABORT_DELAY);
+  });
 }
 
 function handleBrowserRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
+  remixContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
     const { pipe, abort } = renderToPipeableStream(
@@ -85,29 +90,29 @@ function handleBrowserRequest(
       />,
       {
         onShellReady() {
-          const body = new PassThrough()
+          const body = new PassThrough();
 
-          responseHeaders.set('Content-Type', 'text/html')
+          responseHeaders.set('Content-Type', 'text/html');
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
               status: responseStatusCode,
-            }),
-          )
+            })
+          );
 
-          pipe(body)
+          pipe(body);
         },
         onShellError(error: unknown) {
-          reject(error)
+          reject(error);
         },
         onError(error: unknown) {
-          console.error(error)
-          responseStatusCode = 500
+          console.error(error);
+          responseStatusCode = 500;
         },
-      },
-    )
+      }
+    );
 
-    setTimeout(abort, ABORT_DELAY)
-  })
+    setTimeout(abort, ABORT_DELAY);
+  });
 }
